cd11a8ddcd35d4d6e6b7986c5215936ae57b157a
Matt Downs <mattdowns6@gmail.com>
2018-05-07 22:10:34 +1000

    App drawer works!

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import EventTimeline from '../containers/EventTimeline';
 import Grid from 'material-ui/Grid';
 import Drawer from 'material-ui/Drawer';
@@ -10,6 +10,10 @@ import Divider from 'material-ui/Divider';
 import { withStyles } from 'material-ui/styles';
 import DirectionsRun from '@material-ui/icons/DirectionsRun';
 import RssFeed from '@material-ui/icons/RssFeed';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Hidden from 'material-ui/Hidden';
+
 
 
 const drawerWidth = 240;
@@ -20,34 +24,45 @@ const styles = theme => ({
         display: 'flex',
     },
     appBar: {
-        zIndex: theme.zIndex.drawer + 1,
+        zIndex: theme.zIndex.modal + 1,
+    },
+    navIconHide: {
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
     },
     drawerPaper: {
         width: drawerWidth,
     },
     content: {
-        marginLeft: drawerWidth,
         flexGrow: 1,
         backgroundColor: theme.palette.background.default,
         padding: theme.spacing.unit * 3,
+        [theme.breakpoints.up('md')]: {
+            marginLeft: drawerWidth,
+        },
     },
     toolbar: theme.mixins.toolbar,
 });
 
 
-const App = (props) => {
-    const { classes } = props;
+class App extends React.Component {
 
-    return (
-        <div className={classes.root}>
-            <AppBar className={classes.appBar}>
-                <Toolbar>
-                    <Typography variant="title" color="inherit" noWrap>
-                        Parkrun Plus
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" classes={{ paper: classes.drawerPaper }}>
+    state = {
+        mobileOpen: false,
+    };
+
+    handleDrawerToggle = () => {
+        this.setState({ mobileOpen: !this.state.mobileOpen });
+    };
+
+    render() {
+        const { classes, theme } = this.props;
+
+        console.log(theme);
+
+        const drawerContent = (
+            <Fragment>
                 <div className={classes.toolbar} />
                 <List component="nav">
                     <ListItem button>
@@ -63,15 +78,51 @@ const App = (props) => {
                         <ListItemText primary="Athletes" />
                     </ListItem>
                 </List>
-            </Drawer>
-            <main className={classes.content}>
-                <div className={classes.toolbar} />
-                <Grid container>
-                    <EventTimeline/>
-                </Grid>
-            </main>
-        </div>
-    )
+            </Fragment>
+        )
+
+        return (
+            <div className={classes.root}>
+                <AppBar className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton color="inherit" aria-label="open drawer" onClick={this.handleDrawerToggle} className={classes.navIconHide}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="title" color="inherit" noWrap>
+                            Parkrun Plus
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+                <Hidden mdUp>
+                    <Drawer
+                        variant="temporary"
+                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                        open={this.state.mobileOpen}
+                        onClose={this.handleDrawerToggle}
+                        classes={{paper: classes.drawerPaper}}
+                        ModalProps={{
+                            keepMounted: true, // Better open performance on mobile.
+                        }}
+                    >
+                        {drawerContent}
+                    </Drawer>
+                </Hidden>
+                <Hidden smDown implementation="css">
+                    <Drawer variant="permanent" open classes={{ paper: classes.drawerPaper }}>
+                        {drawerContent}
+                    </Drawer>
+                </Hidden>
+
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <Grid container>
+                        <EventTimeline/>
+                    </Grid>
+                </main>
+            </div>
+        );
+    }
 }
 
-export default withStyles(styles)(App)
+export default withStyles(styles, { withTheme: true })(App)
