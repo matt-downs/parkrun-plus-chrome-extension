11dd5621c99cad031a8638436f2d0c3b12bb8665
Matt Downs <mattdowns6@gmail.com>
2018-05-13 21:04:01 +1000

    Initial data is now pulled via http request

--- src/reducers/athletes.js
+++ src/reducers/athletes.js
@@ -2,7 +2,7 @@ const initialState = {};
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case "GOT_ATHLETE": {
+        case "FETCHED_ATHLETE_DATA": {
             let newState =  { ...state };
             newState[action.athlete.athleteId] = action.athlete;
             return newState;
